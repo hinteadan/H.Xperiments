4e9192b24e61b037724bc09e641a843d9654d618
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-07-24 22:59:14 +0300

    # Conflicts: #	H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/CLIDependencyGroup.cs #	H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/H.Qubiz.Xperiments.CLI.csproj #	H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/Properties/launchSettings.json #	H.Qubiz.Xperiments/H.Qubiz.Xperiments.RavenDb/H.Qubiz.Xperiments.RavenDb.csproj #	H.Qubiz.Xperiments/H.Qubiz.Xperiments.SQLite/H.Qubiz.Xperiments.SQLite.csproj #	H.Qubiz.Xperiments/H.Qubiz.Xperiments.sln

---
+++ H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/Commands/UiCliCommand.cs
@@ -0,0 +1,47 @@
+﻿using H.Necessaire;
+using H.Necessaire.Runtime.CLI.Commands;
+using Spectre.Console;
+using System.Threading;
+using System.Threading.Tasks;
+using Spectre.Console.Json;
+using System.Text;
+using H.Necessaire.Serialization;
+using System;
+
+namespace H.Qubiz.Xperiments.CLI.Commands
+{
+    [ID("ui")]
+    internal class UiCliCommand : CommandBase
+    {
+        public override async Task<OperationResult> Run()
+        {
+            await AnsiConsole
+                .Progress()
+                .Columns(new ProgressColumn[]
+    {
+        new TaskDescriptionColumn(),    // Task description
+        new ProgressBarColumn(),        // Progress bar
+        new PercentageColumn(),         // Percentage
+        new RemainingTimeColumn(),      // Remaining time
+        new SpinnerColumn(),            // Spinner
+    })
+                .StartAsync(async ctx => {
+
+                    var t1 = ctx.AddTask("[yellow]Task 1[/]");
+                    var t2 = ctx.AddTask("[green]Task 2[/]");
+
+                    while (!ctx.IsFinished)
+                    {
+                        await Task.Delay(100);
+                        t1.Increment(.1);
+                        t2.Increment(.13);
+                    }
+                });
+
+            Console.WriteLine("Done");
+
+
+            return OperationResult.Win();
+        }
+    }
+}
