19b07f67f0cea3bf5799682621e406b566cda371
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-07-25 22:57:54 +0300

    OD: CLI Xperiments

--- H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/Commands/HelpCommand.cs
+++ H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/Commands/HelpCommand.cs
@@ -9,13 +9,20 @@ namespace H.Qubiz.Xperiments.CLI.Commands
     [Alias("?")]
     internal class HelpCommand : CommandBase
     {
+        static readonly string[] usageSyntax = [
+            "help|?",
+            "help|? [searchKey:string]",
+            "help|? [q=searchKey:string]",
+        ];
+        protected override string[] GetUsageSyntaxes() => usageSyntax;
+
         public override async Task<OperationResult> Run()
         {
             Note[] args = (await GetArguments()).Jump(1);
 
             string searchKey
                 = args.Length == 1
-                ? args.Single().ID
+                ? (args.Single().Value.IsEmpty() ? args.Single().ID : args.Single().Value)
                 : args.Get("q", ignoreCase: true)
                 ;
 
