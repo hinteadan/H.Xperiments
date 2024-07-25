f6df9ed102af0b8bfbef7f9fb0856d5d9949b7a6
Dan Hintea <dan.hintea@qubiz.com>
2024-07-25 15:13:58 +0300

    OD: CLI Xperiments

--- H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/Commands/HelpCommand.cs
+++ H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/Commands/HelpCommand.cs
@@ -1,6 +1,7 @@
 ﻿using H.Necessaire;
 using H.Necessaire.Runtime.CLI.Commands;
 using H.Qubiz.Xperiments.CLI.BLL;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace H.Qubiz.Xperiments.CLI.Commands
@@ -8,13 +9,21 @@ namespace H.Qubiz.Xperiments.CLI.Commands
     [Alias("?")]
     internal class HelpCommand : CommandBase
     {
-        public override Task<OperationResult> Run()
+        public override async Task<OperationResult> Run()
         {
-            CliCommandHelpInfo[] commandsToShowHelpFor = CliCommandsIndexer.AllKnownCliCommands;
+            Note[] args = (await GetArguments()).Jump(1);
+
+            string searchKey
+                = args.Length == 1
+                ? args.Single().ID
+                : args.Get("q", ignoreCase: true)
+                ;
+
+            CliCommandHelpInfo[] commandsToShowHelpFor = CliCommandsIndexer.FindCliCommands(searchKey);
 
             commandsToShowHelpFor.PrintToConsole();
 
-            return OperationResult.Win().AsTask();
+            return OperationResult.Win();
         }
     }
 }
