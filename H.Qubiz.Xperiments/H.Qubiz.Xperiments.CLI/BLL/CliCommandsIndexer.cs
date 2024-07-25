778254d4de9153e0594e8222ef260e5ee3c48cb4
Dan Hintea <dan.hintea@qubiz.com>
2024-07-25 11:17:12 +0300

    OD: CLI Xperiments

--- H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/BLL/CliCommandsIndexer.cs
+++ H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/BLL/CliCommandsIndexer.cs
@@ -60,42 +60,4 @@ namespace H.Qubiz.Xperiments.CLI.BLL
             return commandName.ToLowerInvariant();
         }
     }
-
-    internal class CliCommandHelpInfo
-    {
-        public Type ConcreteType { get; set; }
-        public string Name { get; set; }
-        public string ID { get; set; }
-        public string[] Aliases { get; set; }
-        public string[] Categories { get; set; }
-
-        public string GetPreferredCommandSyntax()
-        {
-            return GetAllCommandSyntaxes(isFullTypeNameIncluded: true).First();
-        }
-
-        public string[] GetAllCommandSyntaxes(bool isFullTypeNameIncluded = false)
-        {
-            List<string> allSyntaxes = new List<string>();
-
-            if (!ID.IsEmpty())
-                allSyntaxes.Add(ID);
-
-            if (Aliases != null)
-                allSyntaxes.AddRange(Aliases.Where(a => !a.IsEmpty()));
-
-            if (!Name.IsEmpty())
-                allSyntaxes.Add(Name);
-
-            if (isFullTypeNameIncluded && !Name.Is(ConcreteType.Name))
-                allSyntaxes.Add(ConcreteType.Name);
-
-            return allSyntaxes.ToArray();
-        }
-
-        public override string ToString()
-        {
-            return $"{string.Join(" | ", GetAllCommandSyntaxes())} [{ConcreteType.TypeName()}]";
-        }
-    }
 }
