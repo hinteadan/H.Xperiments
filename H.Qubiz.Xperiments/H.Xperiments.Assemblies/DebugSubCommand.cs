1c852971e538ae12c1dca2b266e5b3abf6c87cd8
Dan Hintea <dan.hintea@qubiz.com>
2024-07-25 10:20:35 +0300

    merge pending assembly debug changes

--- H.Qubiz.Xperiments/H.Xperiments.Assemblies/DebugSubCommand.cs
+++ H.Qubiz.Xperiments/H.Xperiments.Assemblies/DebugSubCommand.cs
@@ -1,5 +1,7 @@
 ﻿using H.Necessaire;
 using H.Necessaire.Runtime.CLI.Commands;
+using System.Reflection;
+using System.Runtime.Loader;
 
 namespace H.Xperiments.Assemblies
 {
@@ -10,7 +12,20 @@ namespace H.Xperiments.Assemblies
             Log($"Running {GetType().Name}...");
             using (new TimeMeasurement(x => Log($"DONE Running {GetType().Name} in {x}")))
             {
-                
+                AssemblyLoadContext assemblyLoadContextDefault = AssemblyLoadContext.Default;
+                AssemblyLoadContext assemblyLoadContextA = new AssemblyLoadContext("A", isCollectible: true);
+                AssemblyLoadContext assemblyLoadContextB = new AssemblyLoadContext("B", isCollectible: true);
+
+                //assemblyLoadContextA.LoadFromAssemblyPath(@"C:\Wrk\Playground\H.Xperiments\H.Qubiz.Xperiments\H.Qubiz.Xperiments.RavenDb\bin\Debug\net8.0\H.Qubiz.Xperiments.RavenDb.dll");
+                var assembly = assemblyLoadContextA.LoadFromAssemblyName(new AssemblyName("H.Qubiz.Xperiments.RavenDb"));
+
+                var xxx = assemblyLoadContextDefault.LoadFromAssemblyName(new AssemblyName("H.Qubiz.Xperiments.RavenDb"));
+
+                var types = assembly.GetTypes();
+
+                var sharedAssemblies = assemblyLoadContextDefault.Assemblies.ToArray();
+                var assembliesA = assemblyLoadContextA.Assemblies.ToArray();
+                var assembliesB = assemblyLoadContextB.Assemblies.ToArray();
             }
 
             return OperationResult.Win();
