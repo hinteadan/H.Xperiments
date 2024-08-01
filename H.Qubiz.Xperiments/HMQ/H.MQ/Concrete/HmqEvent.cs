488b937d1559d4ee57f1bf56b66f61a8df5ca446
Dan Hintea <dan.hintea@qubiz.com>
2024-08-01 13:02:04 +0300

    OD: CLI MQ Xperiments

---
+++ H.Qubiz.Xperiments/HMQ/H.MQ/Concrete/HmqEvent.cs
@@ -0,0 +1,26 @@
+﻿using H.MQ.Abstractions;
+using H.Necessaire;
+using System;
+
+namespace H.MQ.Concrete
+{
+    internal class HmqEvent : ImAnHmqEvent
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+
+        public DateTime HappenedAt { get; set; } = DateTime.UtcNow;
+
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string Assembly { get; set; }
+
+
+        public Note[] Attributes { get; set; }
+
+
+        public object Data { get; set; }
+    }
+}
---
+++ H.Qubiz.Xperiments/HMQ/H.MQ/HmqExtensions.cs
@@ -0,0 +1,31 @@
+﻿using H.MQ.Abstractions;
+using H.MQ.Concrete;
+using H.Necessaire;
+using System;
+
+namespace H.MQ
+{
+    public static class HmqExtensions
+    {
+        public static ImAnHmqEvent ToHmqEvent<T>(this T data, params Note[] attributes)
+        {
+            Type dataType = data?.GetType() ?? typeof(T);
+
+            return
+                new HmqEvent
+                {
+                    ID = Guid.NewGuid(),
+
+                    HappenedAt = DateTime.UtcNow,
+
+                    Assembly = dataType.Assembly.FullName,
+                    Type = dataType.FullName,
+                    Name = dataType.Name,
+                    
+                    Attributes = attributes.NullIfEmpty(),
+
+                    Data = data,
+                };
+        }
+    }
+}
