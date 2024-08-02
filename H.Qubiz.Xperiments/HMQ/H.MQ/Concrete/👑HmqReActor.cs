876c93df7f911ee66dd9b9b1283856b5775450bc
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-08-02 13:52:38 +0300

    OD: CLI MQ Xperiments

--- H.Qubiz.Xperiments/HMQ/H.MQ/Concrete/👑HmqReActor.cs
+++ H.Qubiz.Xperiments/HMQ/H.MQ/Concrete/👑HmqReActor.cs
@@ -1,20 +1,19 @@
 ﻿using H.MQ.Abstractions;
 using H.Necessaire;
-using System;
 using System.Threading.Tasks;
 
 namespace H.MQ.Concrete
 {
     internal class HmqReActor : HmqActorIdentity, ImAnHmqReActor, ImADependency
     {
-        public Task<OperationResult> Handle(HmqEvent hmqEvent)
-        {
-            throw new NotImplementedException();
-        }
-
         public void ReferDependencies(ImADependencyProvider dependencyProvider)
         {
-            throw new NotImplementedException();
+            
+        }
+
+        public async Task<OperationResult> Handle(HmqEvent hmqEvent)
+        {
+            
         }
     }
 }
