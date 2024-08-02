282e09bcdc81a4daf7b65eacfa17bc624b7f9eb5
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-08-02 20:56:47 +0300

    OD: CLI MQ Xperiments

--- H.Qubiz.Xperiments/HMQ/H.MQ/Concrete/♻️📨PeriodicPollingHmqExternalEventReceiver.cs
+++ H.Qubiz.Xperiments/HMQ/H.MQ/Concrete/♻️📨PeriodicPollingHmqExternalEventReceiver.cs
@@ -1,4 +1,6 @@
-﻿using H.Necessaire;
+﻿using H.MQ.Abstractions;
+using H.Necessaire;
+using System;
 using System.Threading.Tasks;
 
 namespace H.MQ.Concrete
@@ -7,20 +9,60 @@ namespace H.MQ.Concrete
     [Alias("Periodic-Polling", "polling", "poll", "pull")]
     internal class PeriodicPollingHmqExternalEventReceiver : ImAnHmqExternalEventListener, ImADependency
     {
+        static readonly TimeSpan eventRetryMinIterval = TimeSpan.FromSeconds(5);
+        static readonly TimeSpan eventRetryMaxIterval = TimeSpan.FromMinutes(5);
+        static readonly TimeSpan eventRetryIncrement = TimeSpan.FromSeconds(5);
+
+        static readonly TimeSpan pollingInterval = TimeSpan.FromSeconds(5);
+        static readonly TimeSpan intervalToLookBackInto = TimeSpan.FromDays(21);
         ImAnHmqEventRegistry eventRegistry;
+        ImAPeriodicAction poller;
+        ImALogger logger;
         public void ReferDependencies(ImADependencyProvider dependencyProvider)
         {
             eventRegistry = dependencyProvider.Get<ImAnHmqEventRegistry>();
+            poller = poller ?? dependencyProvider.Get<ImAPeriodicAction>();
+            logger = dependencyProvider.GetLogger<PeriodicPollingHmqExternalEventReceiver>();
         }
 
-        public async Task<OperationResult> Start()
+        public Task<OperationResult> Start()
         {
-            throw new System.NotImplementedException();
+            poller.Start(pollingInterval, RunPollingSession);
+            return OperationResult.Win().AsTask();
         }
 
-        public async Task<OperationResult> Stop()
+        public Task<OperationResult> Stop()
         {
-            throw new System.NotImplementedException();
+            poller.Stop();
+            return OperationResult.Win().AsTask();
+        }
+
+        async Task RunPollingSession()
+        {
+            OperationResult<IDisposableEnumerable<HmqEvent>> eventsStreamResult
+                = await eventRegistry.Stream(new HmqEventFilter
+                { 
+                    From = DateTime.UtcNow - intervalToLookBackInto,
+                });
+
+            if (!eventsStreamResult.IsSuccessful)
+            {
+                await logger.LogError($"Error ocurred while trying to stream events from the past {intervalToLookBackInto.TotalDays} days", new OperationResultException(eventsStreamResult), payload: null, eventsStreamResult.FlattenReasons().ToNotes().NullIfEmpty());
+                return;
+            }
+
+            using (IDisposableEnumerable<HmqEvent> stream = eventsStreamResult.Payload)
+            {
+                foreach (HmqEvent hmqEvent in stream)
+                {
+                    await RaiseEvent(hmqEvent);
+                }
+            }
+        }
+
+        async Task RaiseEvent(HmqEvent hmqEvent)
+        {
+            
         }
     }
 }
