fc9d737948726599f6db0c0921d6898512e4dc8c
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-08-03 14:59:23 +0300

    OD: CLI MQ Xperiments

--- H.Qubiz.Xperiments/HMQ/H.MQ.Core/HmqActorIdentityJsonDeserializer.cs
+++ H.Qubiz.Xperiments/HMQ/H.MQ.Core/HmqActorIdentityJsonDeserializer.cs
@@ -1,34 +1,11 @@
 ﻿using H.MQ.Abstractions;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using System;
 
 namespace H.MQ.Core
 {
-    internal class HmqActorIdentityJsonDeserializer : JsonConverter
+    internal class HmqActorIdentityJsonDeserializer : CustomCreationConverter<ImAnHmqActorIdentity>
     {
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            throw new NotSupportedException("HmqActorIdentityJsonConverter should only be used while deserializing.");
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            if (reader.TokenType == JsonToken.Null)
-            {
-                return null;
-            }
-
-            HmqActorIdentity value = serializer.Deserialize<HmqActorIdentity>(reader);
-
-            return value;
-        }
-
-        public override bool CanConvert(Type objectType)
-        {
-            return objectType == typeof(ImAnHmqActorIdentity);
-        }
-
-        public override bool CanWrite => false;
+        public override ImAnHmqActorIdentity Create(Type objectType) => new HmqActorIdentity();
     }
 }
