40764c68f3be737d08121b8674a9bcbdcf2adc27
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-08-04 11:40:44 +0300

    OD: CLI MQ Xperiments

---
+++ H.Qubiz.Xperiments/HMQ/H.MQ.RavenDB/Concrete/ServiceBusMessage.cs
@@ -0,0 +1,16 @@
+﻿using H.MQ.Abstractions;
+using H.Necessaire;
+using System;
+
+namespace H.MQ.RavenDB.Concrete
+{
+    internal class ServiceBusMessage : IStringIdentity
+    {
+        public string ID => $"ServiceBusMessage/{(Event?.ID) ?? Guid.NewGuid()}";
+
+        public HmqEvent Event { get; set; }
+
+        public static implicit operator ServiceBusMessage(HmqEvent @event) => new ServiceBusMessage { Event = @event };
+        public static implicit operator HmqEvent(ServiceBusMessage serviceBusMessage) => serviceBusMessage?.Event;
+    }
+}
--- H.Qubiz.Xperiments/HMQ/H.MQ.RavenDB/Concrete/Storage/HmqEventsRavenDbStorageService.cs
+++ H.Qubiz.Xperiments/HMQ/H.MQ.RavenDB/Concrete/Storage/HmqEventsRavenDbStorageService.cs
@@ -2,52 +2,51 @@
 using H.Necessaire.RavenDB;
 using Raven.Client.Documents.Indexes;
 using Raven.Client.Documents.Linq;
-using System;
 using System.Linq;
 
 namespace H.MQ.RavenDB.Concrete.Storage
 {
     internal class HmqEventsRavenDbStorageService
-        : RavenDbStorageResourceBase<Guid, HmqEvent, HmqEventFilter, HmqEventsFilterIndex>
+        : RavenDbStorageResourceBase<string, ServiceBusMessage, HmqEventFilter, HmqEventsFilterIndex>
     {
-        protected override Guid GetIdFor(HmqEvent item) => item.ID;
+        protected override string GetIdFor(ServiceBusMessage item) => item.ID;
 
-        protected override IRavenQueryable<HmqEvent> ApplyFilter(IRavenQueryable<HmqEvent> query, HmqEventFilter filter)
+        protected override IRavenQueryable<ServiceBusMessage> ApplyFilter(IRavenQueryable<ServiceBusMessage> query, HmqEventFilter filter)
         {
             if (filter?.IDs?.Any() == true)
-                query = query.Where(x => x.ID.In(filter.IDs));
+                query = query.Where(x => x.Event.ID.In(filter.IDs));
 
             if (filter?.From != null)
-                query = query.Where(x => x.HappenedAt >= filter.From);
+                query = query.Where(x => x.Event.HappenedAt >= filter.From);
 
             if (filter?.To != null)
-                query = query.Where(x => x.HappenedAt <= filter.To);
+                query = query.Where(x => x.Event.HappenedAt <= filter.To);
 
             if (filter?.Names?.Any() == true)
-                query = query.Where(x => x.Name.In(filter.Names));
+                query = query.Where(x => x.Event.Name.In(filter.Names));
 
             if (filter?.Types?.Any() == true)
-                query = query.Where(x => x.Type.In(filter.Types));
+                query = query.Where(x => x.Event.Type.In(filter.Types));
 
             if (filter?.Assemblies?.Any() == true)
-                query = query.Where(x => x.Assembly.In(filter.Assemblies));
+                query = query.Where(x => x.Event.Assembly.In(filter.Assemblies));
 
             return query;
         }
     }
 
-    internal class HmqEventsFilterIndex : AbstractIndexCreationTask<HmqEvent>
+    internal class HmqEventsFilterIndex : AbstractIndexCreationTask<ServiceBusMessage>
     {
         public HmqEventsFilterIndex()
         {
             Map = docs => docs.Select(doc =>
                 new
                 {
-                    doc.ID,
-                    doc.HappenedAt,
-                    doc.Name,
-                    doc.Type,
-                    doc.Assembly,
+                    doc.Event.ID,
+                    doc.Event.HappenedAt,
+                    doc.Event.Name,
+                    doc.Event.Type,
+                    doc.Event.Assembly,
                 }
             );
         }
