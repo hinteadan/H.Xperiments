d275986a248c45e9c0c0a6bebcbbd6825ec5b1e3
Dan Hintea <dan.hintea@qubiz.com>
2024-08-05 15:41:27 +0300

    OD: CLI MQ Xperiments

--- H.Qubiz.Xperiments/HMQ/H.MQ.Runtime.RavenDb/Concrete/Storage/RavenDbHmqEventStorageService.cs
+++ H.Qubiz.Xperiments/HMQ/H.MQ.Runtime.RavenDb/Concrete/Storage/RavenDbHmqEventStorageService.cs
@@ -1,10 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using H.MQ.Abstractions;
+using H.Necessaire;
+using H.Necessaire.Runtime.RavenDB;
+using Raven.Client.Documents.Indexes;
+using System;
+using System.Linq;
 
 namespace H.MQ.Runtime.RavenDb.Concrete.Storage
 {
-    internal class RavenDbHmqEventStorageService
+    internal class RavenDbHmqEventStorageService : RavenDbStorageServiceBase<Guid, HmqEvent, HmqEventFilter, HmqEventFilterIndex>
     {
+        protected override TDocQuery ApplyFilterGeneric<TDocQuery>(TDocQuery result, HmqEventFilter filter)
+        {
+            if (filter?.IDs?.Any() ?? false)
+            {
+                result = result.WhereIn(nameof(HmqEvent.ID), filter.IDs.ToStringArray());
+            }
+
+            if (filter?.From != null)
+            {
+                result = result.WhereGreaterThanOrEqual(nameof(HmqEvent.HappenedAt), filter.From.Value);
+            }
+
+            if (filter?.To != null)
+            {
+                result = result.WhereLessThanOrEqual(nameof(HmqEvent.HappenedAt), filter.To.Value);
+            }
+
+            if (filter?.Names?.Any() ?? false)
+            {
+                result = result.WhereIn(nameof(HmqEvent.Name), filter.Names);
+            }
+
+            if (filter?.Types?.Any() ?? false)
+            {
+                result = result.WhereIn(nameof(HmqEvent.Type), filter.Types);
+            }
+
+            if (filter?.Assemblies?.Any() ?? false)
+            {
+                result = result.WhereIn(nameof(HmqEvent.Assembly), filter.Assemblies);
+            }
+
+            return result;
+        }
+    }
+
+    internal class HmqEventFilterIndex : AbstractIndexCreationTask<HmqEvent>
+    {
+        public static readonly HmqEventFilterIndex Instance = new HmqEventFilterIndex();
+
+        public HmqEventFilterIndex()
+        {
+            Map = docs => docs.Select(doc =>
+                new
+                {
+                    doc.ID,
+                    doc.HappenedAt,
+                    doc.Name,
+                    doc.Type,
+                    doc.Assembly,
+                }
+            );
+        }
     }
 }
