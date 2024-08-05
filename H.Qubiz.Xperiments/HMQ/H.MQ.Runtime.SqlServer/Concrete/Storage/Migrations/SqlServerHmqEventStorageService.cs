2ee35d4fe0ac349adcc396d8ba3e56bd4a70afa3
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-08-05 18:44:53 +0300

    OD: CLI MQ Xperiments

---
+++ H.Qubiz.Xperiments/HMQ/H.MQ.Runtime.SqlServer/Concrete/Storage/Migrations/SqlServerHmqEventStorageService.cs
@@ -0,0 +1,42 @@
+﻿using H.MQ.Abstractions;
+using H.Necessaire;
+using H.Necessaire.Dapper;
+
+namespace H.MQ.Runtime.SqlServer.Concrete.Storage.Migrations
+{
+    internal partial class SqlServerHmqEventStorageService
+    {
+        const string tableName = "H.Necessaire.HMQ.HmqEvent";
+
+        static readonly SqlMigration[] sqlMigrations = new SqlMigration[] {
+            new SqlMigration
+            {
+                ResourceIdentifier = "HmqEvent",
+                VersionNumber = new VersionNumber(1, 0),
+                SqlCommand = $@"
+CREATE TABLE [dbo].[{tableName}]
+(
+    [{nameof(HmqEventSqlEntry.ID)}] [uniqueidentifier] NOT NULL,
+    [{nameof(HmqEventSqlEntry.HappenedAt)}] [datetime2](7) NOT NULL,
+    [{nameof(HmqEventSqlEntry.RaisedByJson)}] [nvarchar](MAX) NULL,
+    [{nameof(HmqEventSqlEntry.RaisedByID)}] [nvarchar](450) NULL,
+    [{nameof(HmqEventSqlEntry.Name)}] [nvarchar](450) NULL,
+    [{nameof(HmqEventSqlEntry.Type)}] [nvarchar](450) NULL,
+    [{nameof(HmqEventSqlEntry.Assembly)}] [nvarchar](450) NULL,
+    [{nameof(HmqEventSqlEntry.AttributesJson)}] [nvarchar](MAX) NULL,
+    [{nameof(HmqEventSqlEntry.DataJson)}] [nvarchar](MAX) NULL,
+
+    {nameof(HmqEvent.ID).PrintColumnAsPrimaryKeyConstraintSqlScriptOn(tableName)}
+)
+ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+{nameof(HmqEventSqlEntry.HappenedAt).PrintColumnIndexCreationSqlScriptOn(tableName)}
+{nameof(HmqEventSqlEntry.RaisedByID).PrintColumnIndexCreationSqlScriptOn(tableName)}
+{nameof(HmqEventSqlEntry.Name).PrintColumnIndexCreationSqlScriptOn(tableName)}
+{nameof(HmqEventSqlEntry.Type).PrintColumnIndexCreationSqlScriptOn(tableName)}
+{nameof(HmqEventSqlEntry.Assembly).PrintColumnIndexCreationSqlScriptOn(tableName)}
+",
+            }
+        };
+    }
+}
---
+++ H.Qubiz.Xperiments/HMQ/H.MQ.Runtime.SqlServer/Concrete/Storage/SqlServerHmqEventStorageService.cs
@@ -0,0 +1,34 @@
+﻿using H.Necessaire.Dapper;
+using H.Necessaire;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using H.MQ.Abstractions;
+
+namespace H.MQ.Runtime.SqlServer.Concrete.Storage
+{
+    internal partial class SqlServerHmqEventStorageService
+    {
+    }
+
+    internal class HmqEventSqlEntry : SqlEntryBase, IGuidIdentity
+    {
+        public Guid ID { get; set; }
+        public DateTime HappenedAt { get; set; }
+        public string RaisedByJson { get; set; }
+        public string RaisedByID { get; set; }
+
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string Assembly { get; set; }
+
+
+        public string AttributesJson { get; set; }
+
+
+        public string DataJson { get; set; }
+    }
+}
