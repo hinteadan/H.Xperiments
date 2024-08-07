5f06f5de773771868aa3e98bc4a9b9a0a3aa7167
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-08-07 12:14:09 +0300

    OD: CLI MQ Xperiments

--- H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/CLIDependencyGroup.cs
+++ H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/CLIDependencyGroup.cs
@@ -35,6 +35,8 @@ namespace H.Qubiz.Xperiments.CLI
                 //.WithHmqRabbitMqMessageBus()
                 //.StartHmqRabbitMqExternalListener()
 
+                .WithHNecessaireWebSockets(() => "ws://localhost:11080")
+
                 .Register<RavenDb.DependencyGroup>(() => new RavenDb.DependencyGroup())
                 .Register<SQLite.DependencyGroup>(() => new SQLite.DependencyGroup())
                 .Register<BLL.CliCommandsIndexer>(() => new BLL.CliCommandsIndexer())
@@ -42,26 +44,6 @@ namespace H.Qubiz.Xperiments.CLI
                 .Register<H.Xperiments.CLI.UI.DependencyGroup>(() => new H.Xperiments.CLI.UI.DependencyGroup())
 
                 ;
-
-            IServiceProvider serviceProvider = new ServiceProvider(dependencyRegistry);
-
-            Necessaire.WebSockets.DependencyInjectionExtensions.AddHNecessaireWebSockets(
-                singletonRegistrarByType: t => dependencyRegistry.Register(t, () => Activator.CreateInstance(t)),
-                singletonRegistrarByInterfaceType: (i, t) => dependencyRegistry.Register(i, () => Activator.CreateInstance(t)),
-                singletonRegistrarByFactory: (t, f) => dependencyRegistry.Register(t, () => f.Invoke(serviceProvider)),
-                webSocketServerBaseUrlProvider: x => "ws://localhost:11080"
-                );
-        }
-
-        class ServiceProvider : IServiceProvider
-        {
-            readonly ImADependencyProvider dependencyProvider;
-            public ServiceProvider(ImADependencyProvider dependencyProvider)
-            {
-                this.dependencyProvider = dependencyProvider;
-            }
-
-            public object GetService(Type serviceType) => dependencyProvider.Get(serviceType);
         }
     }
 }
--- H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/Commands/WebSocketCommand.cs
+++ H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/Commands/WebSocketCommand.cs
@@ -2,6 +2,9 @@
 using H.Necessaire.Runtime.CLI.Commands;
 using H.Necessaire.WebSockets;
 using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,33 +13,102 @@ namespace H.Qubiz.Xperiments.CLI.Commands
     [Alias("ws")]
     internal class WebSocketCommand : CommandBase
     {
-        IWebSocketServerService webSocketServerService;
         public override void ReferDependencies(ImADependencyProvider dependencyProvider)
         {
             base.ReferDependencies(dependencyProvider);
 
-            webSocketServerService = dependencyProvider.Get<IWebSocketServerService>();
+            State.WebSocketServerService = dependencyProvider.Get<IWebSocketServerService>();
         }
 
-        public override async Task<OperationResult> Run()
+        public override Task<OperationResult> Run() => RunSubCommand();
+
+        [ID("send")]
+        class SendSubCommand : SubCommandBase
         {
-            Log("WebSocket-ing...");
-            using (new TimeMeasurement(x => Log($"DONE WebSocket-ing in {x}")))
+            IWebSocketServerToClientOperations clientNotifier;
+            public override void ReferDependencies(ImADependencyProvider dependencyProvider)
             {
-                await Task.CompletedTask;
-
-                await webSocketServerService.StartAsync(CancellationToken.None);
-
-                Log("Running WSS Server on ws://localhost:11080");
-
-                Console.ReadLine();
-
-                await webSocketServerService.StopAsync(CancellationToken.None);
-
-                Log("Stopped WSS Server");
+                base.ReferDependencies(dependencyProvider);
+                clientNotifier = dependencyProvider.Get<IWebSocketServerToClientOperations>();
             }
 
-            return OperationResult.Win();
+            public override async Task<OperationResult> Run(params Note[] args)
+            {
+                Log("Sending WS Message...");
+                using (new TimeMeasurement(x => Log($"DONE Sending WS Message in {x}")))
+                {
+                    if (!State.IsRunning)
+                        return OperationResult.Win();
+
+                    await clientNotifier.Broadcast(
+                        new NotificationMessage { 
+                            Content = args?.FirstOrDefault().ID,
+                            Encoding = Encoding.UTF8,
+                            ContentType = NotificationMessageContentType.Plain,
+                            Subject = null,
+                        },
+                        new NotificationAddress { 
+                            Address = "ws://localhost:11080",
+                            Name = "WsDevTesting",
+                        }
+                    );
+
+                    Log("Running WSS Server on ws://localhost:11080");
+                }
+
+                return OperationResult.Win();
+            }
+        }
+
+        [ID("start")]
+        class StartSubCommand : SubCommandBase
+        {
+            public override async Task<OperationResult> Run(params Note[] args)
+            {
+                Log("Starting WS Server...");
+                using (new TimeMeasurement(x => Log($"DONE Starting WS Server in {x}")))
+                {
+                    await State.WebSocketServerService.StartAsync(CancellationToken.None);
+
+                    State.IsRunning = true;
+
+                    Log("Running WSS Server on ws://localhost:11080");
+                }
+
+                return OperationResult.Win();
+            }
+        }
+
+        [ID("stop")]
+        class StopSubCommand : SubCommandBase
+        {
+            public override async Task<OperationResult> Run(params Note[] args)
+            {
+                Log("Stopping WS Server...");
+                using (new TimeMeasurement(x => Log($"DONE Stopping WS Server in {x}")))
+                {
+                    await State.WebSocketServerService.StopAsync(CancellationToken.None);
+
+                    State.Clear();
+
+                    Log("Running WSS Server on ws://localhost:11080");
+                }
+
+                return OperationResult.Win();
+            }
+        }
+
+
+        static class State
+        {
+            public static IWebSocketServerService WebSocketServerService = null;
+
+            public static bool IsRunning { get; set; } = false;
+
+            public static void Clear()
+            {
+                IsRunning = false;
+            }
         }
     }
 }
