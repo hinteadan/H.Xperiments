1f13641412c5f3b12eeafdd28aecd5ea8934c02b
Dan Hintea <dan.hintea@qubiz.com>
2024-08-14 13:01:14 +0300

    OD: CLI MQ Xperiments

--- H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/Commands/DebugCommand.cs
+++ H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/Commands/DebugCommand.cs
@@ -1,5 +1,7 @@
 using H.Necessaire;
 using H.Necessaire.Runtime.CLI.Commands;
+using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -7,8 +9,6 @@ namespace H.Qubiz.Xperiments.CLI.Commands
 {
     internal class DebugCommand : CommandBase
     {
-        SemaphoreSlim semaphoreSlim = new SemaphoreSlim(0);
-
         public override async Task<OperationResult> Run()
         {
             Log("Debugging...");
@@ -17,10 +17,72 @@ namespace H.Qubiz.Xperiments.CLI.Commands
                 await Task.Delay(0);
                 Log("Debug Command");
 
-
+                await foreach(string mess in new InfiniteEnumerable<string>(async x => { await Task.Delay(Random.Shared.Next(500, 1500)); return x.ToString(); }, (m, x) => (x == 5).AsTask()))
+                {
+                    Log(mess);
+                }
             }
 
             return OperationResult.Win();
+        }
+
+        async IAsyncEnumerable<string> DummyStream()
+        {
+            int index = 10;
+
+            while (index >= 0)
+            {
+                index--;
+
+                await Task.CompletedTask;
+                yield return $"{index}";
+            }
+        }
+
+        class InfiniteEnumerator<T> : IAsyncEnumerator<T>
+        {
+            readonly Func<int, Task<T>> valueFactory;
+            readonly Func<T, int, Task<bool>> endOfStreamChecker;
+            int currentIteration = -1;
+            public InfiniteEnumerator(Func<int, Task<T>> valueFactory, Func<T, int, Task<bool>> endOfStreamChecker)
+            {
+                this.valueFactory = valueFactory ?? throw new ArgumentNullException(nameof(valueFactory));
+                this.endOfStreamChecker = endOfStreamChecker ?? throw new ArgumentNullException(nameof(endOfStreamChecker));
+            }
+
+            public T Current { get; private set; }
+
+            public ValueTask DisposeAsync() => ValueTask.CompletedTask;
+
+            public async ValueTask<bool> MoveNextAsync()
+            {
+                currentIteration++;
+
+                T value = await valueFactory(currentIteration);
+                bool isEndOfStream = await endOfStreamChecker(value, currentIteration);
+                if (isEndOfStream)
+                    return false;
+
+                Current = value;
+
+                return true;
+            }
+        }
+
+        class InfiniteEnumerable<T> : IAsyncEnumerable<T>
+        {
+            readonly Func<int, Task<T>> valueFactory;
+            readonly Func<T, int, Task<bool>> endOfStreamChecker;
+            public InfiniteEnumerable(Func<int, Task<T>> valueFactory, Func<T, int, Task<bool>> endOfStreamChecker)
+            {
+                this.valueFactory = valueFactory ?? throw new ArgumentNullException(nameof(valueFactory));
+                this.endOfStreamChecker = endOfStreamChecker ?? throw new ArgumentNullException(nameof(endOfStreamChecker));
+            }
+
+            public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+            {
+                return new InfiniteEnumerator<T>(valueFactory, endOfStreamChecker);
+            }
         }
     }
 }
