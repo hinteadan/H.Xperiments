1ab1af23fd2dd0e642fe7f291e7e1615ba5cf54d
Dan Hintea <dan.hintea@qubiz.com>
2024-08-27 12:27:41 +0300

    OD: Polly Xperiments

---
+++ H.Qubiz.Xperiments/H.Qubiz.Xperiments.CLI/Commands/PollyCommand.cs
@@ -0,0 +1,22 @@
+﻿using H.Necessaire;
+using H.Necessaire.Runtime.CLI.Commands;
+using System.Threading.Tasks;
+
+namespace H.Qubiz.Xperiments.CLI.Commands
+{
+    internal class PollyCommand : CommandBase
+    {
+        public override async Task<OperationResult> Run()
+        {
+            await Task.CompletedTask;
+
+            Log($"Running Polly Command");
+            using (new TimeMeasurement(x => Log($"DONE Running Polly Command in {x}")))
+            {
+
+            }
+
+            return OperationResult.Win();
+        }
+    }
+}
