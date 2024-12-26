3871f42fb024378ad35f2c464c43260b4c7f22d9
Hintee <hintea_dan@yahoo.co.uk>
2024-12-26 11:56:41 +0200

    OD: ThreadingCommand

--- H.Qubiz.Xperiments/H.Xperiments.DotNetStuff/ThreadingCommand.cs
+++ H.Qubiz.Xperiments/H.Xperiments.DotNetStuff/ThreadingCommand.cs
@@ -1,11 +1,15 @@
 ﻿using H.Necessaire;
 using H.Necessaire.CLI.Commands;
+using System.Threading.Tasks;
+using System.Timers;
 
 namespace H.Xperiments.DotNetStuff
 {
     [Alias("th")]
     internal class ThreadingCommand : CommandBase
     {
+        private readonly Timer timer = new Timer { AutoReset = false };
+
         public override async Task<OperationResult> Run()
         {
             Log("Running DotNet Threading Command...");
